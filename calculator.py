2a53e5c744a0e14107151699efdbfd571c424daf
Majid Siddiqui <97821088+majid9971@users.noreply.github.com>
2024-11-18 13:49:04 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,3 +4,9 @@ def calculator():
 
 if __name__ == "__main__":
     calculator()
+    
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Subtraction Result:", subtract(5, 3))
